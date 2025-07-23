9c01d084f2f0adf390b978f0bd07faafc282aa1b
SChavavt <134571321+SChavavt@users.noreply.github.com>
2025-07-23 10:08:38 -0600

    Update app_a-d.py

--- app_a-d.py
+++ app_a-d.py
@@ -714,8 +714,6 @@ def mostrar_pedido(df, idx, row, orden, origen_tab, current_main_tab_label, work
 
         # --- Editable Notes Field and Comment ---
         # This section provides fields for notes and comments related to the order
-        # st.markdown("---")  # Eliminar o comentar esta
-        st.markdown("<div style='margin-top: 4px;'></div>", unsafe_allow_html=True)
         info_text_comment = row.get("Comentario")
         if pd.notna(info_text_comment) and str(info_text_comment).strip() != '':
             st.info(f"💬 Comentario: {info_text_comment}")
